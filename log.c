dd04ac3c571bb53caa2a059918e8d494a9077128
wenzong <565737064@qq.com>
2019-04-01 02:29:33 -0400

    add there lines

--- log.c
+++ log.c
@@ -4,3 +4,8 @@ chsh -s /bin/bash
 2019/03/30
 vim /etc/ssh/sshd_config 
 ClientAliveInterval  60   --应该是需要重启,本地端口也是一样的
+2019/04/01
+echo '' >> test.c
+set .vimrc http://wiki.csie.ncku.edu.tw/vim/vimrc
+python using window10
+  -->> https://blog.csdn.net/Alger_magic/article/details/79674404
