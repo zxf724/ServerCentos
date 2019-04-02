d2fc49593d27bb8d82e56100f6bc6bf49385eec4
wenzong <565737064@qq.com>
2019-04-02 05:53:14 -0400

    add log.c

--- log.c
+++ log.c
@@ -9,3 +9,8 @@ echo '' >> test.c
 set .vimrc http://wiki.csie.ncku.edu.tw/vim/vimrc
 python using window10
   -->> https://blog.csdn.net/Alger_magic/article/details/79674404
+2019/04/02
+解决vi/vim中粘贴会在行首多很多缩进和空格的问题
+https://www.cnblogs.com/end/archive/2012/06/01/2531142.html
+输入前--> set paste
+输入后-->	set nopaste
