fa5e623709e291c69d51ed73140c72551b7da76c
wenzong <565737064@qq.com>
2019-04-22 23:31:19 -0400

    add new video

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+void main() {
+	uint8_t hex = 0x14;
+	uint8_t decimal = 0;
+	decimal = strtol(hex,NULL,16);
+	printf("decimal = %d",decimal);
+}
