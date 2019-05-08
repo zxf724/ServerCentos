d06a69a46b658d0257175faeb247e2d05c40d74d
wenzong <565737064@qq.com>
2019-05-08 04:13:57 -0400

    hex -> decimal %d (int)

--- test1.c
+++ test1.c
@@ -6,13 +6,7 @@ void get_time(uint8_t data[]) {
 	uint32_t localtime = 0;
 	localtime = (data[2] << 24) | (data[3] << 16) | (data[4] << 8) | data[5];
 	printf("localtime = 0x%08x \n",localtime);
-	// hex -> DEC
-	char hex[4] = {0};
-	for(uint8_t i=0;i<=4;i++) {
-		hex[i] = data[i+2];
-	}
-	uint32_t decimal = strtol(hex,NULL,16);
-	printf("convert to decimal : %d \n",decimal);
+	printf("localtime = %d \n",localtime);  //hex -> decemal
 }
 
 void main() {
