a4e0e553cdb321db6f92f7940f89b0fcee4c1a92
wenzong <565737064@qq.com>
2019-05-08 03:59:47 -0400

    gcc --std=c99

--- test1.c
+++ test1.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <math.h>
-#include <string.h>
+#include <stdlib.h>
 
 void get_time(uint8_t data[]) {
 	uint32_t localtime = 0;
-	localtime = (data[1] << 24) | (data[2] << 16) | (data[3] << 8) | data[4];
+	localtime = (data[2] << 24) | (data[3] << 16) | (data[4] << 8) | data[5];
 	printf("localtime = 0x%08x \n",localtime);
 	// hex -> DEC
-	uint8_t len = strlen(localtime);
-//	uint8_t i = 0;
-	long sum = 0;
-	for(uint8_t i=0;i<len;i++) {
-		if((char)localtime[i] >= 'A' && (char)localtime[i] <= 'Z')
-			(char)localtime[i] = int((char)localtime[i] - 'A') + 10 + '0';
-		sum += ((char)localtime[i] - '0') * pow(16,len-1-i);
+	char hex[4] = {0};
+	for(uint8_t i=0;i<=4;i++) {
+		hex[i] = data[i+2];
 	}
-	printf("localtime = %ld \n",sum);
+	uint32_t decimal = strtol(hex,NULL,16);
+	printf("convert to decimal : %d \n",decimal);
 }
 
 void main() {
